45d17e2629e9c986e64e3d8f446ab673f4f018bd
Rebanta Chakraborty <rebantachakraborty8@gmail.com>
2022-07-02 23:22:09 +0530

    add password-manager-v2

--- Day 30 - Password Manager v2 - Save JSON, Errors, Exception/password-manager-v2/main.py
+++ Day 30 - Password Manager v2 - Save JSON, Errors, Exception/password-manager-v2/main.py
@@ -1,96 +1,94 @@
-import tkinter
+from tkinter import *
 from tkinter import messagebox
-import random
+from random import choice, randint, shuffle
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# Password Generator Project
+
 
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-    nr_letters = random.randint(8, 10)
-    nr_symbols = random.randint(2, 4)
-    nr_numbers = random.randint(2, 4)
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
 
-    password_list = []
-
-    password_letters = [random.choice(letters) for _ in range(nr_letters)]
-    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
-    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
     password_list = password_letters + password_symbols + password_numbers
-
-    random.shuffle(password_list)
+    shuffle(password_list)
 
     password = "".join(password_list)
-
-    password_input.insert(0, password)
+    password_entry.insert(0, password)
     pyperclip.copy(password)
-
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+
 def save():
-    website = website_input.get()
-    username = email_input.get()
-    password = password_input.get()
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    new_data = {
+        website: {
+            "email" : email,
+            "password" : password,
+        }
+    }
 
-    if(len(website) and len(username) and len(password)):
-        is_ok = messagebox.askokcancel(
-            title=website, message=f"Details entered: \nEmail: {username} \nPassword: {password} \nClick Ok to confirm")
-
-        if is_ok:
-            with open("data.txt", mode="a") as file1:
-                file1.write(f"{website} | {username} | {password}\n")
-                website_input.delete(0, "end")
-                password_input.delete(0, "end")
-    else:
+    if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(
-            title="Oops", message="Don't leave any fields empty!")
+            title="Oops", message="Please make sure you haven't left any fields empty.")
+    else:
+        with open("data.json", "r") as data_file:
+            # json.dump(new_data, data_file, indent=4) # to write to a json file
+            data = json.load(data_file) # to read from a json file
+            data.update(new_data) # to update a existing json file
+
+        with open("data.json", "w") as data_file:
+            json.dump(data, data_file, indent=4) # to write the updated data into the json file
+
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
-
-window = tkinter.Tk()
-window.title("PASSWORD MANAGER")
+window = Tk()
+window.title("Password Manager")
 window.config(padx=50, pady=50)
 
-# Lock Img
-canvas = tkinter.Canvas(width=200, height=200)
-lock_img = tkinter.PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=lock_img)
-canvas.grid(column=1, row=0)
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
 
-# Website Tag:
-website_label = tkinter.Label(text="Website:")
-website_label.grid(column=0, row=1)
+# Labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
 
-website_input = tkinter.Entry(width=40)
-website_input.grid(column=1, row=1, columnspan=2)
-website_input.focus()  # focuses the cursor on the entry
+# Entries
+website_entry = Entry(width=40)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+email_entry = Entry(width=40)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "rebantachakraborty8@gmail.com")
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
 
-# Email Tag:
-email_label = tkinter.Label(text="Email/Username:")
-email_label.grid(column=0, row=2)
-
-email_input = tkinter.Entry(width=40)
-email_input.grid(column=1, row=2, columnspan=2)
-email_input.insert(0, "rebantachakraborty8@gmail.com")
-
-# Email Tag:
-password_label = tkinter.Label(text="Password:")
-password_label.grid(column=0, row=3)
-
-password_input = tkinter.Entry(width=22)
-password_input.grid(column=1, row=3)
-
-generate_button = tkinter.Button(text="Generate Password", command=generate_password)
-generate_button.grid(column=2, row=3)
-
-# Add button
-add_button = tkinter.Button(text="Add", width=35, command=save)
-add_button.grid(column=1, row=4, columnspan=2)
-
+# Buttons
+generate_password_button = Button(
+    text="Generate Password", command=generate_password)
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=35, command=save)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()

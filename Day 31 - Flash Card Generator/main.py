36aea42b36f6056386ba45a734430d99513a666f
Rebanta Chakraborty <rebantachakraborty8@gmail.com>
2022-07-03 18:44:01 +0530

    add flip card function

--- Day 31 - Flash Card Generator/main.py
+++ Day 31 - Flash Card Generator/main.py
@@ -7,12 +7,24 @@ BACKGROUND_COLOR = "#B1DDC6"
 data = pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
 # print(to_learn)
+current_card = {}
 
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer) # so that the card is flipped only when the user waits on a card for 4s.
+
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    window.after(4000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
 
 
 #------------------------------------- UI DESIGN ------------------------------------------#
@@ -20,10 +32,13 @@ window = tkinter.Tk()
 window.title("FLASH CARD GENERATOR")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(4000, func=flip_card)
+
 canvas = tkinter.Canvas(width=800, height=526)
-# Card Front
+# Card Img
 card_front_img = tkinter.PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = tkinter.PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
 
 card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
